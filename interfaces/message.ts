b0033aa4406e70267f6049380271ea60883ef18d
Gabriel_ConteGrand <gabrielcontegrand10@gmail.com>
2022-10-28 11:22:33 -0300

    tsconfig

---
+++ interfaces/message.ts
@@ -0,0 +1,6 @@
+export interface IMessage {
+  _id?: string
+  text: string
+  createdAt?: string
+  updatedAt?: string
+}
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,8 +4,8 @@ export const formatDate = (allMessages: Message[]) => {
   const updatedMessages = allMessages.map((msg: Message) => {
     let newDate = new Date().toDateString().slice(0, -5)
 
-    const hours = new Date(msg.createdAt).getHours()
-    let minutes = new Date(msg.createdAt).getMinutes().toString()
+    const hours = new Date(msg.createdAt!).getHours()
+    let minutes = new Date(msg.createdAt!).getMinutes().toString()
 
     if (minutes.toString().length === 1) {
       minutes = '0' + minutes
@@ -13,7 +13,7 @@ export const formatDate = (allMessages: Message[]) => {
     newDate = newDate + ', ' + hours + ':' + minutes
 
     const horaJS = new Date().toLocaleTimeString()
-    const mongoAFormatoHora = new Date(msg.createdAt).toLocaleTimeString()
+    const mongoAFormatoHora = new Date(msg.createdAt!).toLocaleTimeString()
 
     // if(newDate[0] + newDate[1] === horaJS[0] + horaJS[1]){
     //     const minutes = Number(newDate[2] + newDate[])
